9b6620008d9ac4464d26795a53ec4d751ad484d8
Bill Xiong <xiong.bill@yahoo.com>
2021-01-21 22:08:20 -0800

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValues) => {
+  const [darkMode, setDarkMode] = useLocalStorage("dark", initialValues);
+
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+  };
+  return [darkMode, toggleMode];
+};
+export default useDarkMode;
